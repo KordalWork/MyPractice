a6279ae5b643de90d0a0b8da55cc39ab7ce57c72
unknown <ega_13@mail.ru>
2021-01-25 17:42:44 +0300

    include drowser-sync and write gilp tusks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-import gulp from 'gulp';
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
 
 gulp.task('hello', function(done) {
-  consile.log ('Helo world');
+  console.log('Hello World');
   done();
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
