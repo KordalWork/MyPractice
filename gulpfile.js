b4a964c23d52b74b705efafd42526758b3864f5c
unknown <ega_13@mail.ru>
2021-01-26 23:17:23 +0300

    connect browser-sync plugin and write function in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 
 const scss = require('gulp-sass');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
+function browsersync() {
+  browserSync.init({
+    server : {
+      baseDir: 'src/'
+    }
+  });
+}
 
 
 function styles() {
@@ -10,11 +18,16 @@ function styles() {
       .pipe(scss({outputStyle: 'compressed'}))
       .pipe(concat('style.min.css'))
       .pipe(dest('src/css'))
+      .pipe(browserSync.stream())
 }
 
 function watching() {
   watch(['src/scss/**/*.scss'], styles);
+  watch(['src/*html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(browsersync, watching);
