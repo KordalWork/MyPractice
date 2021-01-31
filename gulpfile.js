727fdefbbd859d0282e7df0e4c357634f7544330
unknown <ega_13@mail.ru>
2021-01-31 13:55:06 +0300

    refact: rewrite cod in gulpfile change scss to sass file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, watch, parallel } = require('gulp');
 
-const scss = require('gulp-sass');
+const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
@@ -14,15 +14,15 @@ function browsersync() {
 
 
 function styles() {
-  return src('src/scss/style.scss')
-      .pipe(scss({outputStyle: 'compressed'}))
+  return src('src/scss/style.sass')
+      .pipe(sass({outputStyle: 'compressed'}))
       .pipe(concat('style.min.css'))
       .pipe(dest('src/css'))
       .pipe(browserSync.stream())
 }
 
 function watching() {
-  watch(['src/scss/**/*.scss'], styles);
+  watch(['src/scss/**/*.sass'], styles);
   watch(['src/*html']).on('change', browserSync.reload);
 }
 
