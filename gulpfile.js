9037c54d7ebee9f349a54519037661d1e573d268
unknown <ega_13@mail.ru>
2021-02-10 14:12:24 +0300

    add gulp-autoprefexer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const { src, dest, watch, parallel } = require('gulp');
 
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const concat       = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync  = require('browser-sync').create();
 
 function browsersync() {
   browserSync.init({
@@ -17,6 +18,10 @@ function styles() {
   return src('src/sass/style.sass')
       .pipe(sass({outputStyle: 'compressed'}))
       .pipe(concat('style.min.css'))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true
+      }))
       .pipe(dest('src/css'))
       .pipe(browserSync.stream())
 }
